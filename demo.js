d6d3c953cebe1872df74d6da2ba3e9bd0c0f8dff
mr. suthar <shiv.str21@gmail.com>
2022-06-11 11:51:26 +0530

    initial commit

---
+++ demo.js
@@ -0,0 +1,120 @@
+// console.log("Hello It's Mr. suthar");
+// // alert("I am belog to Rj");
+
+// // creating variable
+// // var name="shiv";
+
+// // Displaying  value
+// // document.write(name);
+
+// //Method or Function
+// function myname(name, age)
+// {
+//     document.write(name+" & "+age );
+    
+// }
+// // myname("shiv",21);
+
+
+// //variable
+// /*
+// There  are two types of variable i.e primatative and non-primetive 
+// primative -
+// 1  String 
+// 2 number (int) 
+// 3 Boolean
+// 4  undefined
+// 5 null
+
+// where non- primative exits with 
+// Array
+// objects
+// RegExp
+// */
+
+// const car=["audi","BMW","Honda","tata"];
+// const num=[1,2,3,45];
+// // document.write(num);
+
+
+// // instance of Array
+// // 1st way
+// const car2=new Array();
+// car2[0]=1;
+// car2[1]=2;
+// car2[2]=3;
+// car2[3]=4;
+// // document.write(car2);
+
+// //2nd way
+// const car3=new Array();
+// // document.write(car3);
+
+//   const person={first:"shiv", last:"suthar"}  ;
+
+// //   document.write(person.last);
+
+// //   document.write(person.first);
+
+
+//     var days=3;
+//     switch(days){
+//         case 1: "Monday";
+//         break;
+//         case 2:" Tuesday";
+//         break;
+//         case 3: "Wednesday";
+//         break;
+//         case 4: "Thursday";
+//         break;
+//         case 5:"Friday" ;
+//         break;
+//         case 6: "Saturday";
+//         break;
+//         case 7: "Sunday";
+//         break;
+// default:
+//      "holiday";
+//     break;
+
+    
+//     }
+//     document.write(days);
+
+// //events
+// // 1. onclick
+// function Click(){
+//   // alert("hello");
+// }
+
+// //String
+// // You can use let or var
+// var  text1="Good ";
+// var text2="Morning";
+
+// document.write(text1.slice(1,-3));
+
+// function change(){
+//   alert("This is required");
+// }
+
+// let t1="Good     ";
+// let t2="Morning";
+// // alert(t1.concat(t2));
+// // alert(t1.concat(t2).concat(" shiv"));
+// // alert(t1.concat(t2). toLocaleUpperCase());
+// // window.alert(t1.indexOf("o"));
+// //  alert(t1.split());
+
+ 
+// //  function Click(){
+// //   var t=confirm("are you sure");
+// //   if(t==true){
+// // alert("Thank you!")
+
+// //     }
+// //   }
+
+
+
+
---
+++ mgs.js
@@ -0,0 +1,5 @@
+function mgs() {
+  var v = prompt("Who are you");
+  alert("I am " + v);
+
+}
